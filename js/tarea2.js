54ac0136d01734c74f505e617e165f1254137433
ValenAnna <valenannacondia@gmail.com>
2022-07-20 22:06:19 -0300

    first commit

---
+++ js/tarea2.js
@@ -0,0 +1,9 @@
+let texto = "hola"
+
+do {
+    let numero = parseInt(prompt("Introduce un numero",0))
+    for(let i=0;i<=numero;i++){
+        document.writeln(texto);
+        }
+
+}while(numero >= 0)
