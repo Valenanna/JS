4d8083c2f2a424594ed8ced945cbcf5c58787839
Valenanna <106562914+Valenanna@users.noreply.github.com>
2022-07-25 20:56:27 -0300

    Update tarea2.js

--- js/tarea2.js
+++ js/tarea2.js
@@ -2,7 +2,7 @@ let texto = "hola"
 
 do {
     let numero = parseInt(prompt("Introduce un numero",0))
-    for(let i=0;i<=numero;i++){
+    for(let i=0;i=numero;i++){
         document.writeln(texto);
         }
 
