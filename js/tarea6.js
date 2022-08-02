9933814ea47d487dc0f599af952c8c35b63105c7
Valenanna <106562914+Valenanna@users.noreply.github.com>
2022-08-02 00:03:25 -0300

    Add files via upload

---
+++ js/tarea6.js
@@ -0,0 +1,31 @@
+
+class autos{
+    constructor(id,marcaAuto,modeloAuto,añoAuto,colorAuto){
+        this.id = id,
+        this.marcaAuto = marcaAuto,
+        this.modeloAuto = modeloAuto,
+        this.añoAuto = añoAuto,
+        this.colorAuto = colorAuto
+    
+    }
+
+    mostrarInformacion(){
+        console.log(`El autos es marca ${this.marcaAuto}, modelo ${modeloAuto}, del año ${añoAuto} y su color es ${this.colorAuto}`)
+    }
+}
+
+let desguace = []
+
+function nuevoAuto(){
+    let marcaAuto = prompt("Ingrese la marca del auto")
+    let modeloAuto = prompt("Ingrese el modelo del auto")
+    let añoAuto = parseInt(prompt("Ingrese el año (de fabricacion) del auto"))
+    let colorAuto = prompt("Ingrese el color del auto")
+
+    let autoIngresado = new autos(desguace.length+1,marcaAuto,modeloAuto,añoAuto,colorAuto)
+    console.log(autoIngresado)
+    desguace.push(autoIngresado)
+}
+
+nuevoAuto("");
+
